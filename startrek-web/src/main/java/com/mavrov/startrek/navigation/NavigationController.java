3a2cced5e5240cb2583b7167a98ababec242e6a7
sergii.mavrov <serg.mavrov@gmail.com>
2015-10-23 19:45:57 +0200

    main fe (just example)

--- startrek-web/src/main/java/com/mavrov/startrek/navigation/NavigationController.java
+++ startrek-web/src/main/java/com/mavrov/startrek/navigation/NavigationController.java
@@ -1,8 +1,10 @@
 package com.mavrov.startrek.navigation;
 
-import javax.faces.bean.ManagedProperty;
+import org.jboss.logging.Logger;
+
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import java.io.Serializable;
 import java.util.Map;
 
@@ -12,15 +14,22 @@ import java.util.Map;
 @SessionScoped
 public class NavigationController implements Serializable {
 
+    @Inject
+    private transient Logger logger;
+
     public String showPage() {
         FacesContext context = FacesContext.getCurrentInstance();
         Map<String, String> parameters = context.getExternalContext().getRequestParameterMap();
         String email = parameters.get("email");
+        return showPage(email);
+    }
+
+    public String showPage(String email) {
+        logger.info("email parameter=" + email);
         if (!email.isEmpty()) {
             return "profile-view";
         } else {
             return "profile-edit";
         }
     }
-
 }
